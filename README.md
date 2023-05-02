b34c1da09f6d821d44acb1e31c11be302d2232ac
PascalR <24775431+mroxso@users.noreply.github.com>
2023-05-02 09:48:26 +0200

    dev2main (#2)
    * add Docker and Contribution to README.md
    
    * rm not needed stuff
    
    * show polls from nostr
    
    ---------
    
    Co-authored-by: mroxso <mail@pascalrost.de>

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pollstr
 A Poll Website for Nostr
+
+# Docker
+Run pre-built Docker Image:
+```
+docker run --rm -p 8080:80 ghcr.io/mroxso/pollstr
+```
+
+or build it yourself with:
+```
+git clone git@github.com:mroxso/pollstr.git
+cd pollstr
+docker build -t pollstr:local .
+docker run --rm -p 8080:80 pollstr:local
+```
+
+# Contribution
+Feel free to contribute
--- src/script.js
+++ src/script.js
@@ -18,6 +18,24 @@ socket.onmessage = function(event) {
             // console.log(content, pubkeyShortened, formattedTime);
             console.log(content);
             // TODO: Show polls
+            var div = document.createElement('div');
+            div.setAttribute('class', 'col');
+            div.innerHTML = `
+                <div class="card shadow-sm">
+                    <!-- <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg> -->
+                    <div class="card-body">
+                        <p class="card-text">${content}.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary">Yes</button>
+                                <button type="button" class="btn btn-sm btn-outline-secondary">No</button>
+                            </div>
+                            <small class="text-body-secondary">9 mins</small>
+                        </div>
+                    </div>
+                </div>
+            `;
+            document.getElementById('polls-row').appendChild(div);
         }
     } else {
         // infoContainer.innerHTML = data[2].content;
