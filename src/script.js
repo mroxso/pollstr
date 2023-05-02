25ff67ea600f002e74ab93142641f10804d00387
mroxso <mail@pascalrost.de>
2023-05-02 10:32:14 +0200

    improvement of html card building in js

--- src/script.js
+++ src/script.js
@@ -12,38 +12,40 @@ socket.onmessage = function(event) {
             const tags = [];
             for (const tag of data[2].tags) {
                 if (tag[0] === "poll_option") {
-                    // console.log(tag[2]);
                     tags.push(tag[2]);
                 }
             }
-            // console.log(tags);
             const pubkey = data[2].pubkey;
             const pubkeyShortened = `${pubkey.slice(0, 3)}...${pubkey.slice(-3)}`;
             const createdAt = data[2].created_at;
             const date = new Date(createdAt * 1000);
             const formattedTime = date.toLocaleString();
-            // TODO: Show polls
             var divCol = document.createElement('div');
             divCol.setAttribute('class', 'col');
             var divCard = document.createElement('div');
             divCard.setAttribute('class', 'card shadow-sm');
             var divCardBody = document.createElement('div');
             divCardBody.setAttribute('class', 'card-body');
-            
-            divCardBody.innerHTML = `
-                <p class="card-text">${content}.</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        `
-                        for(const tag of tags) {
-                            divCardBody.innerHTML +=
-                            `<button type="button" class="btn btn-sm btn-outline-secondary">${tag}</button>`;
-                        }
-                        `
-                    </div>
-                    <small class="text-body-secondary">${formattedTime}</small>
-                </div>
-            `;
+            var pCardText = document.createElement('p');
+            pCardText.setAttribute('class', 'card-text');
+            pCardText.innerHTML = content;
+            var divBtnFlex = document.createElement('div');
+            divBtnFlex.setAttribute('class', 'd-flex justify-content-between align-items-center');
+            var divBtnGroup = document.createElement('div');
+            divBtnGroup.setAttribute('class', 'btn-group');
+            for(const tag of tags) {
+                divBtnGroup.innerHTML +=
+                `<button type="button" class="btn btn-sm btn-outline-secondary">${tag}</button>`;
+            }
+            var smallText = document.createElement('small');
+            smallText.setAttribute('class', 'text-body-secondary');
+            smallText.innerHTML = formattedTime;
+
+            divBtnFlex.appendChild(divBtnGroup);
+            divBtnFlex.appendChild(smallText);
+            divCardBody.appendChild(pCardText);
+            divCardBody.appendChild(divBtnFlex);
+
             divCard.appendChild(divCardBody);
             divCol.appendChild(divCard);
             document.getElementById('polls-row').appendChild(divCol);
